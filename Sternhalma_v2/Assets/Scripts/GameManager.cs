393b267e1ecfe2de98a0cedc262baca7f0c0b9eb
har2711 <haradhesh@gmail.com>
2024-07-09 14:51:51 -0700

    Piece counter
    Piece counter implemented on all tutorials and levels

--- Sternhalma_v2/Assets/Scripts/GameManager.cs
+++ Sternhalma_v2/Assets/Scripts/GameManager.cs
@@ -219,6 +219,7 @@ public class GameManager : MonoBehaviour
         //List<Vector3> scissorList = new List<Vector3> { new Vector3(0, 0) };
         List<Vector3> scissorList = new List<Vector3> { new Vector3(-3.0f, 1.0f), new Vector3(0, -1.0f) };
         var scissorCount = scissorList.Count;
+        UnitManager.Instance.currentScissorCount = scissorCount;
 
         for (int i = 0; i < scissorCount; i++)
         {
@@ -246,6 +247,7 @@ public class GameManager : MonoBehaviour
         //List<Vector3> rockList = new List<Vector3> { new Vector3(1.5f, -0.5f), new Vector3(-1.5f, -0.5f) };
         List<Vector3> rockList = new List<Vector3> { new Vector3(-1.5f, -1.5f), new Vector3(-1.5f, 0.5f), new Vector3(0.0f, 1.0f), new Vector3(1.5f, -0.5f), new Vector3(3.0f, 0) };
         var rockCount = rockList.Count;
+        UnitManager.Instance.currentRockCount = rockCount;
 
         for (int i = 0; i < rockCount; i++)
         {
@@ -264,6 +266,7 @@ public class GameManager : MonoBehaviour
         //List<Vector3> paperList = new List<Vector3> { new Vector3(1.5f, 0.5f), new Vector3(0, -1), new Vector3(-1.5f, 0.5f) };
         List<Vector3> paperList = new List<Vector3> { new Vector3(-3.0f, 0f), new Vector3(-1.5f, 1.5f), new Vector3(1.5f, 1.5f), new Vector3(1.5f, -1.5f), new Vector3(3.0f, -1.0f) };
         var paperCount = paperList.Count;
+        UnitManager.Instance.currentPaperCount = paperCount;
 
         for (int i = 0; i < paperCount; i++)
         {
@@ -291,6 +294,10 @@ public class GameManager : MonoBehaviour
         UnitManager.Instance.piecesRemoved = 0;                                      // start with all pieces on board; none removed
         UnitManager.Instance.piecesRemovedMeter.SetMaxProgress(UnitManager.Instance.pieceCount - 1);      // win condition requires 1 piece remaining
         UnitManager.Instance.piecesRemovedMeter.SetProgress(UnitManager.Instance.piecesRemoved);
+
+        UnitManager.Instance.rocksLeft.text = UnitManager.Instance.currentRockCount.ToString();
+        UnitManager.Instance.papersLeft.text = UnitManager.Instance.currentPaperCount.ToString();
+        UnitManager.Instance.scissorsLeft.text = UnitManager.Instance.currentScissorCount.ToString();
 
         // Change the game state to PlayerTurn after spawning objects
         GameManager.Instance.ChangeState(GameState.PlayerTurn);
