cb9992395bc928065eeeafab80b149f3b7fbfa5f
RutujaaP <43179702+RutujaaP@users.noreply.github.com>
2024-07-21 15:08:47 -0700

    grid updated

--- Sternhalma_v2/Assets/Scripts/Tut4_GameManager.cs
+++ Sternhalma_v2/Assets/Scripts/Tut4_GameManager.cs
@@ -138,7 +138,7 @@ public class Tut4_GameManager : MonoBehaviour
         
 
         float hexWidth = hexSize + 0.4f;
-        float hexHeight = hexSize * Mathf.Sqrt(3) + 0.4f;
+        float hexHeight = hexSize * Mathf.Sqrt(3) + 0.6f;
 
         for (float x = -3.0f; x <= 3.0f; x += 1.5f)  //overall range of x
         {
