deafd3d48267358110d73a784bf92489b885a8e9
cclai-usc <cclai@usc.edu>
2024-07-24 00:29:17 -0700

    Modify tutorial 3's introductory hints

--- Sternhalma_v2/Assets/Scripts/PerimeterTutorial.cs
+++ Sternhalma_v2/Assets/Scripts/PerimeterTutorial.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class PerimeterTutorial : MonoBehaviour
 {
+    public Button rotatableTutorialButton;
+    public GameObject rotatableTutorialPrefab;
+    public GameObject rotatableInstructions;
+
     public Button perimeterTutorialButton;
     public GameObject perimeterTutorialPrefab;
     public GameObject perimeterInstructions;
@@ -14,9 +18,21 @@ public class PerimeterTutorial : MonoBehaviour
         Time.timeScale = 0f;
         PauseMenu.gameIsPaused = true;
     }
+
     public void dismissTutorial()
     {
-        Debug.Log("dismissed Tutorial!");
+        rotatableTutorialButton.gameObject.SetActive(false);
+        rotatableTutorialPrefab.SetActive(false);
+        rotatableInstructions.SetActive(false);
+
+        perimeterTutorialButton.gameObject.SetActive(true);
+        perimeterTutorialPrefab.SetActive(true);
+        perimeterInstructions.SetActive(true);
+    }
+
+    public void dismissTutorial2()
+    {
+        Debug.Log("dismissed Tutorial2!");
         //perimeterTutorialButton.enabled = false;
         //GetComponent<Button>().enabled = false;
 
